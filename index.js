47c9f9cb90efcc976f747c2025261b5c2ad8c212
Chandru Nagendran <chandru@redblacktree.com>
2023-01-11 18:18:35 +0530

    finished auth flow

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
+const cookieSession =  require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+
+const authRoutes = require('./routes/authRoutes');
 const app=express();
 
-app.get('/',(req,res)=>{
-    res.send({hi :'there'});
-});
+app.use(cookieSession({
+    maxAge : 30 * 24 * 60 * 60* 1000,
+    keys : [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(keys.mongoURL);
+
+authRoutes(app);
+
+// app.get('/',(req,res)=>{
+//     res.send({hi :'there'});
+// });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
