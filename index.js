312ad484f1b25160f2954b5349ba7a4802ebe52c
Chandru Nagendran <chandru@redblacktree.com>
2023-01-18 12:37:34 +0530

    Add vercel build

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ mongoose.connect(keys.mongoURL);
 authRoutes(app);
 billingRoutes(app);
 
+if (process.env.NODE_ENV === 'production'){
+    //express will serve up production assests like our main.js or main.css file
+    app.use(express.static('client/build'));
+
+    //express will surve te index.html file if it doesn't recognize the route
+
+    const path =require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    })
+}
+
 // app.get('/',(req,res)=>{
 //     res.send({hi :'there'});
 // });
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,13 +1,26 @@
-const keys = require('../config/keys');
-const stripe = require('stripe')(keys.stripeSecretKey);
-module.exports = app=>{
-    app.post('/api/stripe', async(req,res)=>{
-        const charge =await stripe.charges.create({
-            amount :500,
-            currency : 'usd',
-            description : '$5 for 5 credits',
-            source : req.body.id
-        });
-        console.log(charge);
+const keys = require("../config/keys");
+const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
+
+module.exports = (app) => {
+  app.post("/api/stripe", requireLogin , async (req, res) => {
+    
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 500,
+      currency: "inr",
+      description: "$5 for 5 credits",
+      payment_method_data: {
+        type: "card",
+        card: {
+          token: req.body.id,
+        },
+      },
+      confirmation_method: "manual",
+      confirm: "true",
     });
-}
+ 
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
+  });
+};
